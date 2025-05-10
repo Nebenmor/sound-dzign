134240e55d94ccbb908be6161bbc051a07f4ac8b
Nebenmor <nebenmor.anthony@gmail.com>
2025-05-10 10:22:34 +0100

    implement project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {
+  Header,
+  Topics,
+  Info,
+  Blog,
+  Testimonials,
+  Footer,
+} from "./components/index";
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
