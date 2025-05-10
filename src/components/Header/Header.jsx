031115223a39aa3343e3c0fbb3e1447d055e9322
Nebenmor <nebenmor.anthony@gmail.com>
2025-05-10 10:43:14 +0100

    implement Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,13 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="wrapper">
+        <p className="course-name">Sound Design Master Class</p>
+        <h1>Learn the Art of Sound Design</h1>
+        <a href="#" className="demo-btn">Demo Lesson</a>
+      </div>
+    </header>
   );
 };
 
