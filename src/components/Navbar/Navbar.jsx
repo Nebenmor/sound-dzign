8956181b00b7c112f28493b0f4fa88debfe13173
Nebenmor <nebenmor.anthony@gmail.com>
2025-05-10 18:02:27 +0100

    implement Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import "./Navbar.css"
+import React from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav>
+      <a href="#" className="logo">
+        Sound<span className="red">DZing</span>
+      </a>
 
-export default Navbar
+      <ul>
+        <li>
+          <a href="#">Coure Details</a>
+          <a href="#">About</a>
+          <a href="#">Blog</a>
+          <a href="#">Testimonials</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
