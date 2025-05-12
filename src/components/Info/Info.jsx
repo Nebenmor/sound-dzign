4876570ecc90fa9f2f0655aef4b11896e658d7f9
Nebenmor <nebenmor.anthony@gmail.com>
2025-05-12 17:01:57 +0100

    implement info component

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import "./Info.css"
+import { bgElement2, studentIcon, videoIcon } from '../../assets' 
 
 const Info = () => {
   return (
-    <div>Info</div>
+    <section id="info" className="dark-gray">
+      <div className="wrapper">
+        <div className="content-container">
+          <div className="info-content">
+            <img src={studentIcon} alt="student-icon" />
+            <div className='amount' >23,000+</div>
+            <div className='type' >students</div>
+          </div>
+
+          <div className="info-content">
+            <img src={studentIcon} alt="student-icon" />
+            <div className='amount' >26 Hrs</div>
+            <div className='type' >Video Content</div>
+          </div>
+        </div>
+
+        <img className='bg-element-2' src={bgElement2} alt="bg-element-2" />
+      </div>
+    </section>
   )
 }
 
