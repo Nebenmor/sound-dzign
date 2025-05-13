c3fe38e7e2342aabf0ccb553aec0a8eeabe1a16a
Nebenmor <nebenmor.anthony@gmail.com>
2025-05-13 06:18:24 +0100

    implement blog section componenet

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,58 @@
-import React from 'react'
-import "./Blog.css"
+import React from "react";
+import "./Blog.css";
+import { blog1, blog2, blog3 } from "../../assets";
 
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <section id="blog" className="pink">
+      <div className="wrapper">
+        <h2 className="light">Latest Posts</h2>
+      <div className="content-container">
+        <div className="post">
+          <div className="tag">DAW</div>
+          <a href="#">
+            <img src={blog1} alt="blog-1" />
+          </a>
 
-export default Blog
+          <a href="">
+            <h3 className="post-title">
+              Now To Use Drum Machine in logic Pro X
+            </h3>
+          </a>
+        </div>
+
+        <div className="post">
+          <div className="tag">Mixing</div>
+          <a href="#">
+            <img src={blog2} alt="blog-1" />
+          </a>
+
+          <a href="">
+            <h3 className="post-title">How To Mix Guitars Effectively</h3>
+          </a>
+        </div>
+
+        <div className="post">
+          <div className="tag">Vox</div>
+          <a href="#">
+            <img src={blog3} alt="blog-1" />
+          </a>
+
+          <a href="#">
+            <h3 className="post-title">
+              The Real Power of Harmonies in Music Production
+            </h3>
+          </a>
+        </div>
+      </div>
+
+      <div className="btn-container">
+        <a href="#" className="all-posts-btn">
+          All Posts</a>
+      </div>
+      </div>
+    </section>
+  );
+};
+
+export default Blog;
